9e344aab903aec8368965a8f2b945bb14cf4911a
Osvaldo Rodriguez <osvaldorodriguezneavez@gmail.com>
2024-08-07 20:59:27 -0600

    front end del index, index funcional

--- prueba.php
+++ prueba.php
@@ -21,6 +21,7 @@ if (isset($_POST['entrar'])) {
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,28 +29,34 @@ if (isset($_POST['entrar'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
+
 <body>
+<section style="background-image: url(img/subscribe-bg.jpg); width: 100%; height: 100vh; background-repeat: no-repeat; background-size: cover;">">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card shadow-2-strong" style="border-radius: 2rem;">
+          <div class="card-body p-5 text-center">
     <div class="prueba">
-        <center>
+        
+            <h2 style="color: red">INICIO DE SESION</h2> <br>
+            
             <form method="post">
-                <h5>Email</h5>
-                <input name="email" placeholder="Email" type="email" style="width: 250px">
+            <label for="exampleInputEmail1" class="form-label"><h5>Email</h5></label> <br>
+            <input name="email" class="form-control" type="email" type="email" aria-describedby="emailHelp">
                 <br>
-                <h5>Password</h5>
-                <input name="password" placeholder="Password" type="password" style="width: 250px">
+            <label for="exampleInputPassword1" class="form-label"><h5>Contraseña</h5></label>
+            <input name="password" type="password" class="form-control" id="exampleInputPassword1">
                 <br>
-                <br>
-                <button type="submit" name="entrar" id="entrar"> Enviar</button>
+                <button type="submit" name="entrar" id="entrar" class="btn btn-primary">Enviar</button>
             </form>
-        </center>
+        
     </div>
-    <style>
-        .prueba {
-            margin-top: 15%;
-        }
-    </style>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
